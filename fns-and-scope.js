f6602dfc7d49763502d77c0301867266d6d8eae8
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-09-04 21:08:43 -0600

    Finished problems one and two. " Ugh." ls

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,13 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+function isTyler(name) {
+  if (name === 'Tyler') {
+    return true;
+  } else {
+    return false;
+  }
+}
 //////////////////PROBLEM 2////////////////////
 
 
@@ -13,8 +19,10 @@ var name = 'Tyler';
 
 
   //Code Here
-
-
+function getName() {
+  var name = prompt("What is your name?");
+  return name;
+}
 //////////////////PROBLEM 3////////////////////
 
 
@@ -53,7 +61,7 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
